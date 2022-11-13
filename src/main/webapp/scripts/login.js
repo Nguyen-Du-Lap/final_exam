8c91d4ac8b0ea449b7b1e612022b1982804f665a
NguyenThiQuynhHuong68 <20130275@st.hcmuaf.edu.vn>
2022-11-14 00:10:25 +0700

    Nguyen Thi Quynh Huong commit

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -7,6 +7,41 @@ form_login.addEventListener('submit', (e) => {
     checkInputs();
 });
 
+// handle onchane 
+
+function emailOnChange(value) {
+    if(value === '') {
+      setError(email, 'Vui lòng nhập vào email');
+    } else if(!isEmail(value)) {
+        setError(email, 'Email không hợp lệ');
+    } else {
+        setSuccess(email);
+    }
+}
+
+function passwordOnChange(value) {
+    if(value === '') {
+        setError(password, 'Vui lòng nhập vào mật khẩu');
+    } else if(value.length < 8) {
+        setError(password, 'Mật khẩu phải có ít nhất 8 kí tự');
+    } else {
+        setSuccess(password);   
+    }
+}
+
+function setError(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'input error';
+}
+function setSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'input success';
+}
+
+
+  // handle when submit
 function checkInputs() {
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
@@ -40,3 +75,4 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
--- src/main/webapp/scripts/signUp.js
+++ src/main/webapp/scripts/signUp.js
@@ -11,6 +11,104 @@ form_signUp.addEventListener('submit', (e) => {
     checkInputs();
 });
 
+//handle onchane
+function emailOnChange(value) {
+    if(value === '') {
+      setError(email, 'Vui lòng nhập vào email');
+    } else if(!isEmail(value)) {
+        setError(email, 'Email không hợp lệ');
+    } else {
+        setSuccess(email);
+    }
+}
+
+var passwordOnchane = ""
+
+function passwordOnChange(value) {
+    passwordOnchane = value
+    if(value === '') {
+        setError(password, 'Vui lòng nhập vào mật khẩu');
+    } else if(value.length < 8) {
+        setError(password, 'Mật khẩu phải có ít nhất 8 kí tự');
+    } else {
+        setSuccess(password);   
+    }
+}
+
+function passwordAOnChange(value) {
+    if(value === '') {
+        setError(password2, 'Vui lòng nhập lại mật khẩu');
+    }
+    if(value.length < 8) {
+        setError(password2, 'Mật khẩu phải có ít nhất 8 kí tự');
+    }
+    else if (value !== passwordOnchane) {
+        setError(password2, "Mật khẩu không khớp");
+    } else {
+        setSuccess(password2);
+    }
+}
+
+function namefOnChange(value) {
+    if(value === '') {
+        setErrorForFName(fname, 'Vui lòng nhập họ của bạn');
+    } else {
+        setSuccessForFName(fname);
+    }
+}
+
+function namelOnChange(value) {
+    if(value === '') {
+        setErrorForLName(lname, 'Vui lòng nhập tên của bạn');
+    } else {
+        setSuccessForLName(lname);
+    }
+}
+
+function phoneOnChange(value) {
+    if(value === '') {
+      setError(phoneNumber, 'Vui lòng nhập số điện thoại của bạn');
+    } else if(!isPhoneNumber(value)) {
+        setError(phoneNumber, 'Số điện thoại không hợp lệ')
+    } else {
+        setSuccess(phoneNumber);
+    }
+}
+
+function setError(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'input error';
+}
+function setSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'input success';
+}
+
+function setErrorForFName(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'input fname error';
+}
+function setSuccessForFName(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'input fname success';
+}
+function setErrorForLName(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'input lname error';
+}
+function setSuccessForLName(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'input lname success';
+}
+
+
+//handle when submit
 function checkInputs() {
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
@@ -97,3 +195,5 @@ function isEmail(email) {
 function isPhoneNumber(phoneNumber) {
     return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(phoneNumber);
 }
+
+
